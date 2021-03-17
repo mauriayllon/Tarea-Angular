2ad2df057178d111ee1e570b158eb39ea4e379b2
Mauricio Ayllon <mauricio.ad1707@gmail.com>
2021-03-17 16:07:03 -0400

    Tarea Angular

--- src/app/componentes-comunes/print-error/print-error.component.ts
+++ src/app/componentes-comunes/print-error/print-error.component.ts
@@ -2,12 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-print-error',
-  template: `
-  <div class="error" *ngIf="control && control.errors && (control.dirty || control.touched)">
-  <div *ngIf="control.errors.required"><small>El campo es requerido</small></div>
-  <div *ngIf="control.errors.email"><small>El correo no cumple con el formato requerido.</small></div>
-</div>
-`,
+  templateUrl: './print-error.component.html',
   styleUrls: ['./print-error.component.scss']
 })
 export class PrintErrorComponent implements OnInit {
